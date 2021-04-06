4f667a2da53d307c1d244a60305bced7b7793844
Domenico Pasquale <domenicopasq@gmail.com>
2021-04-06 16:36:17 +0200

    Delete src directory

--- src/APIKey.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'APIKey' : 'AIzaSyBVRzuhqq-c0C0N6s8ACoK7czgMcRXp06A'
-};
--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const SEARCH_BOOK = 'SEARCH_BOOK';
-export const FETCH_BOOKS = 'FETCH_BOOKS';
-export const FETCH_BOOK = 'FETCH_BOOK';
-export const FETCH_POSTER = 'FETCH_POSTER';
-export const LOADING = 'LOADING';
